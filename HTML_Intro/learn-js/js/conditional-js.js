e230dd34cce821e968d0115bfa832b98e39dceb3
Excel951 <abigailexcelsisdeo@gmail.com>
2022-10-03 22:39:41 +0700

    Learn the Js First

---
+++ HTML_Intro/learn-js/js/conditional-js.js
@@ -0,0 +1,11 @@
+// console.log('BEFORE CONDITIONAL');
+let random = Math.random();
+if (random < 0.5) {
+    console.log('YOUR RANDOM NUMBER IS LESS THAN 0.5');
+    console.log(random);
+}
+if (random >= 0.5) {
+    console.log('YOUR RANDOM NUMBER IS BIGGER THAN 0.5');
+    console.log(random);
+}
+// console.log('AFTER CONDITIONAL');
