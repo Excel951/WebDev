aa0e031d9efe696de2f6937dd7b2c2a02f15303f
Excel951 <abigailexcelsisdeo@gmail.com>
2022-10-04 22:18:44 +0700

    Js

--- HTML_Intro/learn-js/js/running-js.js
+++ HTML_Intro/learn-js/js/running-js.js
@@ -1,3 +1,9 @@
 console.log('HELLO FROM OUR FIRST JS FILE!!!');
 let total = 1 + 3;
 console.log('GOOD BYE!!!');
+
+// Array
+// push - add to end
+// pop - remove from end
+// shift - remove from start
+// unshift - add from start
